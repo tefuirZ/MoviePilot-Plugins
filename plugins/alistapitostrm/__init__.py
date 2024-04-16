57d82175479c1984c1e64fb5d4cbe0d1ea8af91b
tefuirZ <63311404+tefuirZ@users.noreply.github.com>
2024-04-17 02:54:25 +0800

    Update __init__.py

--- plugins/alistapitostrm/__init__.py
+++ plugins/alistapitostrm/__init__.py
@@ -21,7 +21,7 @@ class alistapitostrm(_PluginBase):
     plugin_color = "#3B5E8E"
     plugin_version = "1.0"
     plugin_author = "tefuir"
-    author_url = "https://github.com/tefuirz"
+    author_url = "https://github.com/tefuirZ"
     plugin_config_prefix = "alistapito_strmfile_"
     plugin_order = 30
     auth_level = 1
@@ -48,15 +48,111 @@ class alistapitostrm(_PluginBase):
             thread = threading.Thread(target=self.create_strm_files)
             thread.start()
 
-    def get_state(self) -> bool:
-        return self._enabled
+
+
+
+    def create_strm_files(self):
+        print('脚本运行中。。。。。。。')
+        json_structure = {}
+        base_url = self._site_url + '/d' + self._root_path + '/'
+        self.traverse_directory(self._root_path, json_structure, base_url, self._target_directory)
+        os.makedirs(self._target_directory, exist_ok=True)
+        self.create_strm_files(json_structure, self._target_directory, base_url)
+        print('所有strm文件创建完成')
+
+
+
+    def requests_retry_session(
+        self,
+        retries=3,
+        backoff_factor=0.3,
+        status_forcelist=(500, 502, 504),
+        session=None,
+    ):
+        session = session or requests.Session()
+        retry = Retry(
+            total=retries,
+            read=retries,
+            connect=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=status_forcelist,
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        return session
+
+    def list_directory(self, path):
+        url_list = self._site_url + "/api/fs/list"
+        payload_list = {
+            "path": path,
+            "password": "",
+            "page": 1,
+            "per_page": 0,
+            "refresh": False
+        }
+        headers_list = {
+            'Authorization': self._token,
+            'User-Agent': 'Mozilla/5.0',
+            'Content-Type': 'application/json'
+        }
+        response_list = self.requests_retry_session().post(url_list, headers=headers_list, json=payload_list)
+        return response_list.json()
+
+    def traverse_directory(self, path, json_structure, base_url, target_directory, is_root=True):
+        directory_info = self.list_directory(path)
+        if directory_info.get('data') and directory_info['data'].get('content'):
+            for item in directory_info['data']['content']:
+                if item['is_dir']:
+                    new_path = os.path.join(path, item['name'])
+                    sleep(5)
+                    self.traverse_directory(new_path, json_structure, base_url, target_directory, is_root=False)
+                elif self.is_video_file(item['name']):
+                    json_structure[item['name']] = {
+                        'type': 'file',
+                        'size': item['size'],
+                        'modified': item['modified']
+                    }
+
+        if not is_root:
+            self.create_strm_files(json_structure, target_directory, base_url,
+                                   path.replace(self._root_path, '').strip('/').replace('/', os.sep))
+
+    def is_video_file(self, filename):
+        video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv']
+        return any(filename.lower().endswith(ext) for ext in video_extensions)
+
+    def create_strm_files(self, json_structure, target_directory, base_url, current_path=''):
+        for name, item in json_structure.items():
+            full_path = os.path.join(target_directory, current_path)
+            if isinstance(item, dict) and item.get('type') == 'file':
+                if not item.get('created'):
+                    strm_filename = name.rsplit('.', 1)[0] + '.strm'
+                    strm_path = os.path.join(full_path, strm_filename)
+
+                    if os.path.exists(strm_path):
+                        print(f"{strm_path} 已存在，跳过创建。")
+                        continue
+
+                    os.makedirs(full_path, exist_ok=True)
+                    encoded_file_path = urllib.parse.quote(os.path.join(current_path.replace('\\', '/'), name))
+                    video_url = base_url + encoded_file_path
+                    item['created'] = True
+                    with open(strm_path, 'w', encoding='utf-8') as strm_file:
+                        strm_file.write(video_url)
+                        print(f"{strm_path} 已创建。")
+            elif isinstance(item, dict):
+                new_directory = os.path.join(full_path, name)
+                os.makedirs(new_directory, exist_ok=True)
+                self.create_strm_files(item, target_directory, base_url, os.path.join(current_path, name))
 
     @staticmethod
-    def get_command():
+    def get_command() -> List[Dict[str, Any]]:
         pass
 
-    def get_api(self):
+    def get_api(self) -> List[Dict[str, Any]]:
         pass
+
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         return [
@@ -195,101 +291,11 @@ class alistapitostrm(_PluginBase):
             "token": self._token
         }
 
-    def get_page(self):
+    def get_page(self) -> List[dict]:
         pass
 
-    def create_strm_files(self):
-        print('脚本运行中。。。。。。。')
-        json_structure = {}
-        base_url = self._site_url + '/d' + self._root_path + '/'
-        self.traverse_directory(self._root_path, json_structure, base_url, self._target_directory)
-        os.makedirs(self._target_directory, exist_ok=True)
-        self.create_strm_files(json_structure, self._target_directory, base_url)
-        print('所有strm文件创建完成')
+    def get_state(self) -> bool:
+        return self._enabled
 
     def stop_service(self):
         pass
-
-    def requests_retry_session(
-        self,
-        retries=3,
-        backoff_factor=0.3,
-        status_forcelist=(500, 502, 504),
-        session=None,
-    ):
-        session = session or requests.Session()
-        retry = Retry(
-            total=retries,
-            read=retries,
-            connect=retries,
-            backoff_factor=backoff_factor,
-            status_forcelist=status_forcelist,
-        )
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        return session
-
-    def list_directory(self, path):
-        url_list = self._site_url + "/api/fs/list"
-        payload_list = {
-            "path": path,
-            "password": "",
-            "page": 1,
-            "per_page": 0,
-            "refresh": False
-        }
-        headers_list = {
-            'Authorization': self._token,
-            'User-Agent': 'Mozilla/5.0',
-            'Content-Type': 'application/json'
-        }
-        response_list = self.requests_retry_session().post(url_list, headers=headers_list, json=payload_list)
-        return response_list.json()
-
-    def traverse_directory(self, path, json_structure, base_url, target_directory, is_root=True):
-        directory_info = self.list_directory(path)
-        if directory_info.get('data') and directory_info['data'].get('content'):
-            for item in directory_info['data']['content']:
-                if item['is_dir']:
-                    new_path = os.path.join(path, item['name'])
-                    sleep(5)
-                    self.traverse_directory(new_path, json_structure, base_url, target_directory, is_root=False)
-                elif self.is_video_file(item['name']):
-                    json_structure[item['name']] = {
-                        'type': 'file',
-                        'size': item['size'],
-                        'modified': item['modified']
-                    }
-
-        if not is_root:
-            self.create_strm_files(json_structure, target_directory, base_url,
-                                   path.replace(self._root_path, '').strip('/').replace('/', os.sep))
-
-    def is_video_file(self, filename):
-        video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv']
-        return any(filename.lower().endswith(ext) for ext in video_extensions)
-
-    def create_strm_files(self, json_structure, target_directory, base_url, current_path=''):
-        for name, item in json_structure.items():
-            full_path = os.path.join(target_directory, current_path)
-            if isinstance(item, dict) and item.get('type') == 'file':
-                if not item.get('created'):
-                    strm_filename = name.rsplit('.', 1)[0] + '.strm'
-                    strm_path = os.path.join(full_path, strm_filename)
-
-                    if os.path.exists(strm_path):
-                        print(f"{strm_path} 已存在，跳过创建。")
-                        continue
-
-                    os.makedirs(full_path, exist_ok=True)
-                    encoded_file_path = urllib.parse.quote(os.path.join(current_path.replace('\\', '/'), name))
-                    video_url = base_url + encoded_file_path
-                    item['created'] = True
-                    with open(strm_path, 'w', encoding='utf-8') as strm_file:
-                        strm_file.write(video_url)
-                        print(f"{strm_path} 已创建。")
-            elif isinstance(item, dict):
-                new_directory = os.path.join(full_path, name)
-                os.makedirs(new_directory, exist_ok=True)
-                self.create_strm_files(item, target_directory, base_url, os.path.join(current_path, name))
